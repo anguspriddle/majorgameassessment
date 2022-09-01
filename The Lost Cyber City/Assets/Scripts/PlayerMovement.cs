c42d84b41f35caceac5d1f7648c32f948d619b57
gus <19171@wc.school.nz>
2022-09-01 19:13:58 +1200

    trying to add animations

--- The Lost Cyber City/Assets/Scripts/PlayerMovement.cs
+++ The Lost Cyber City/Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Variables
+    public float PlayerSpeed = 5.0f;
+    public float horizontalInput;
+    public bool onGround = true;
+    private float jumpForce = 4.0f;
+    private float gravityModifier = 1f;
+    public float jumpAmount = 0;
+    private Rigidbody2D playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRb = GetComponent<Rigidbody2D>();
+        Physics.gravity *= gravityModifier;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && jumpAmount < 2) 
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpAmount += 1;
+            onGround = false;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * PlayerSpeed * Time.deltaTime);
+    }
+
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            jumpAmount = 0;
+            onGround = true;
     }
 }
