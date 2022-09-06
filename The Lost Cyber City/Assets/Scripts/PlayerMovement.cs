0efe880363375e8e941a4536b6b32e15da0b1e0e
gus <19171@wc.school.nz>
2022-09-07 11:08:21 +1200

    Created animation code
    Created animation code that works and continued to edit the character sprite sheet to have everything coloured correctly on every frame

--- The Lost Cyber City/Assets/Scripts/PlayerMovement.cs
+++ The Lost Cyber City/Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,8 @@ public class PlayerMovement : MonoBehaviour
     private float gravityModifier = 1f;
     public float jumpAmount = 0;
     private Rigidbody2D playerRb;
-    private Keycode latestkey;
+    private bool Facing_Left;
+    private bool Facing_Right;
     
     // Animations and Animation States
     Animator animator;
@@ -28,29 +29,55 @@ public class PlayerMovement : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody2D>();
         Physics.gravity *= gravityModifier;
-        
+        animator = gameObject.GetComponent<Animator>();
+        ChangeAnimationState(PLAYER_IDLELEFT);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && jumpAmount < 2 && latestkey) 
-        {
-            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jumpAmount += 1;
-            onGround = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && jumpAmount < 2 && latestkey == KeyCode.A or latestkey == KeyCode.LeftArrow) 
-        {
-            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jumpAmount += 1;
-            onGround = false;
-        }
-
-
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * PlayerSpeed * Time.deltaTime);
+        if (horizontalInput == 0 && onGround == true)
+        {
+            if (Facing_Left == true)
+            {
+                ChangeAnimationState(PLAYER_IDLERIGHT);
+            }
+            if (Facing_Right == true)
+            {
+                ChangeAnimationState(PLAYER_IDLELEFT);
+            }
+        }
+        if (horizontalInput > 0 && onGround == true)
+        {
+            ChangeAnimationState(PLAYER_RUNRIGHT);
+            Facing_Right = true;
+            Facing_Left = false;
+        }
+        if (horizontalInput < 0 && onGround == true)
+        {
+            ChangeAnimationState(PLAYER_RUNLEFT);
+            Facing_Right = false;
+            Facing_Left = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && jumpAmount < 2 && Facing_Left == true) 
+        {
+            ChangeAnimationState(PLAYER_JUMPLEFT);
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpAmount += 1;
+            onGround = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpAmount < 2 && Facing_Right == true)
+        {
+            ChangeAnimationState(PLAYER_JUMPRIGHT);
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpAmount += 1;
+            onGround = false;
+        }
+            
+
     }
 
     
@@ -58,5 +85,14 @@ public class PlayerMovement : MonoBehaviour
     {
             jumpAmount = 0;
             onGround = true;
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+
+        animator.Play(newState);
+
+        currentState = newState;
     }
 }
