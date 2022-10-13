85bab3bdaed3604419325e6c1b2d59c705cfc374
gus <19171@wc.school.nz>
2022-10-13 23:45:06 +1300

    Created main menu, buttons and timer text
    Created lots of things and continued creating enemy

--- The Lost Cyber City/Assets/Scripts/EnemyMovement.cs
+++ The Lost Cyber City/Assets/Scripts/EnemyMovement.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    private float enemySpeed;
+    private bool faceRight;
+    public GameObject Waypoint1;
+    public GameObject Waypoint2;
     void Start()
     {
+        faceRight = true;
         enemySpeed = 1.0f;
     }
 
     void Update()
     {
-        transform.Translate(Vector3.right * enemySpeed * Time.deltaTime);
+        if (faceRight == true)
+        {
+            transform.Translate(Vector3.right * enemySpeed * Time.deltaTime);
+        }
+
+        if (faceRight == false)
+        {
+            transform.Translate(Vector3.left * enemySpeed * Time.deltaTime);
+        }
+
+        if (transform.position.x >= Waypoint1.transform.position.x) 
+        {
+            faceRight = false;
+        }
+        if (transform.position.x <= Waypoint2.transform.position.x) 
+        {
+            faceRight = true;
+        }
+
     }
 }
    
--- The Lost Cyber City/Assets/Scripts/GameManager.cs
+++ The Lost Cyber City/Assets/Scripts/GameManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public int lives = 3;
+    public Text timeText;
+    public Text healthText;
+    public float TimeLeft;
+    public float currentTime = 300;
+    public bool TimerOn = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimerOn = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (TimerOn)
+        {
+            if (TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+            }
+            else
+            {
+                TimeLeft = 0;
+                TimerOn = false;
+            }
+
+            currentTime -= Time.deltaTime;
+
+            float minutes = Mathf.FloorToInt((currentTime / 60));
+            float seconds = Mathf.FloorToInt((currentTime % 60));
+            timeText.text = string.Format("Time: {0:00} : {1:00}", minutes, seconds);
+        }
     }
+
 }
